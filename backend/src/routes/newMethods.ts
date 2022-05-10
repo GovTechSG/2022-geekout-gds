f1f5cc5e4b795cd12e52a18bfb5d3b15f36379d1
Damien Sim <damien_SIM@tech.gov.sg>
2022-05-10 14:48:44 +0800

    Checkpoint-3 Implement updateTodoById

--- backend/src/routes/newMethods.ts
+++ backend/src/routes/newMethods.ts
@@ -20,6 +20,15 @@ export async function deleteTodoById(req: Request, res: Response) {
     }
   }
 
-export async function updateTodoById(req: Request, res: Response) {
-    return res.status(501).json({ message: "Not implemented" });
+  export async function updateTodoById(req: Request, res: Response) {
+    const { id } = req.params;
+    const updatedTodo = req.body;
+    if (updatedTodo.id !== id) {
+      return res.status(409).json({message: "UUID in path and body do not match"});
+    } else if (id in todoList) {
+        todoList[id] = updatedTodo;
+        return res.status(200).send();
+    } else {
+        return res.status(400).json({ message: "UUID does not exist" });
+    }
 }
