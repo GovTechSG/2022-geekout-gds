866e658150ebd814fe34ec0a0a0c1c49a38c4b11
Damien Sim <damien_SIM@tech.gov.sg>
2022-05-10 14:39:50 +0800

    Checkpoint-1 Implement getTodoById

--- backend/src/routes/newMethods.ts
+++ backend/src/routes/newMethods.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
-// import {todoList} from './methods'
+import {todoList} from './methods'
 
 export async function getTodoById(req: Request, res: Response) {
-    return res.status(501).json({ message: "Not implemented" });
+    const { id } = req.params;
+    if (id in todoList) {
+      return res.status(200).json(todoList[id]);
+    } else {
+      return res.status(400).json({ message: "UUID does not exist" });
+    }
 }
 
 export async function deleteTodoById(req: Request, res: Response) {
